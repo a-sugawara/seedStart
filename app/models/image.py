4285bacb50c3c36b9ce9818a86a485ef08fa3668
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-06 11:55:33 -0600

    created image branch

--- app/models/image.py
+++ app/models/image.py
@@ -1,1 +1,17 @@
 from .db import db
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key = True)
+    image_url = db.Column(db.Text, nullable = False)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable = False)
+
+    project = db.relationship('Project', back_populates='images')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'image_url': self.image_url,
+            'project_id': self.project_id    
+        }
