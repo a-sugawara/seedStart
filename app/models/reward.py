2c8de7fd4e979ef91623d2644b3ebe32836e0303
adeline <adelineswires@gmail.com>
2021-12-06 09:44:00 -0800

    rewards model and typo edits

--- app/models/reward.py
+++ app/models/reward.py
@@ -1,1 +1,21 @@
 from .db import db
+
+class Reward(db.Model):
+    __tablename__ = 'rewards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text(3000), nullable=False)
+
+    project = db.relationship('Project', back_populates='rewards')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'project_id': self.project_id,
+            'price': self.price,
+            'description': self.description
+        }
