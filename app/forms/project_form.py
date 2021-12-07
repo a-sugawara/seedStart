1158e9eba0d63d5003beaaf6fc0880caa12a642d
Alfredo Sugawara <nicora221@gmail.com>
2021-12-06 19:32:32 -0500

    CR for projects

---
+++ app/forms/project_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email, ValidationError
+from app.models import Project
+
+
+class ProjectForm(FlaskForm):
+    user_id = IntegerField('user_id', validators=[DataRequired()])
+    category_id = IntegerField('category_id', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    goal_amount = IntegerField('goal_amount', validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired()])
