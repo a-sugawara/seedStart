cd92cf41b45c7c7d3937c1f5a64f9fbc1b06a1ac
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-10 13:18:57 -0600

    added a progress bar to the all projects card page

--- react-app/src/components/AllProjectsPage/index.js
+++ react-app/src/components/AllProjectsPage/index.js
@@ -11,12 +11,21 @@ export default function AllProjectsPage(){
     const details = projects?.map((project, idx) =>
     <NavLink key={idx} to={`/projects/${project.id}`}>
         <div className="project-card">
-            <img className="project-card-img" src={project.images[0]}/>
+            <div className="image-bar">
+                <img className="project-card-img" src={project.images[0]}/>
+                <div className="progress-bar-all">
+                    <div className="progress-status-all"
+                    style={{width:`${(project?.backing?.reduce((acc, a)=>acc+a,0)) / (project?.goal_amount) * 100}%`}}></div>
+                </div>
+            </div>
             <div className="project-card-info" >
                 <div className="title">{project.title}</div>
                 <div>Goal: ${project.goal_amount}</div>
-                <div>Raised: ${project.backing.reduce((acc, a)=>acc+a,0)}</div>
-                <div>User: {project.user}</div>
+                {/* <div>Raised: ${project.backing.reduce((acc, a)=>acc+a,0)}</div> */}
+                <div>By {project.user}</div>
+            </div>
+            <div>
+
             </div>
         </div>
     </NavLink>
