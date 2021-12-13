8051c09570eb230ab24a89fe272d2c1a49204ba5
Alfredo Sugawara <nicora221@gmail.com>
2021-12-13 00:31:23 -0500

    tab open on github link

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -6,10 +6,10 @@ export default function Footer(){
     return(
         <div className="footer">
             {/* we can link our githubs/other social media if we want */}
-            <div className="footerPiece"><a href="https://github.com/a-sugawara"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Alfredo</div>
-            <div className="footerPiece"><a href="https://github.com/ad-sw"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Adeline</div>
-            <div className="footerPiece"><a href="https://github.com/Vour123"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Jose</div>
-            <div className="footerPiece"><a href="https://github.com/rcwhite96"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Rachel</div>
+            <div className="footerPiece"><a target="_blank" href="https://github.com/a-sugawara"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Alfredo</div>
+            <div className="footerPiece"><a target="_blank" href="https://github.com/ad-sw"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Adeline</div>
+            <div className="footerPiece"><a target="_blank" href="https://github.com/Vour123"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Jose</div>
+            <div className="footerPiece"><a target="_blank" href="https://github.com/rcwhite96"><img className="github" alt="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" style={{opacity:".4"}}></img></a> Rachel</div>
         </div>
         )
 }
