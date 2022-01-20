6ae0dcd8fa2aa7858ecac61e3e31b9e4d0a9310e
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2022-01-19 21:46:25 -0600

    created post route for likes

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,5 +1,29 @@
 from flask import Blueprint, jsonify, request, session
-from app.models import likes, db
-from app.forms import BackingForm
+from app.models import Likes, db
+from app.forms import LikeForm
 
 likes_routes = Blueprint('likes', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+@likes_routes.route('/', methods=['POST'])
+def post_like():
+    form = LikeForm();
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_like = Likes(
+            project_id = form.data['project_id'],
+            user_id = form.data['user_id']
+        )
+        db.session.add(new_like)
+        db.session.commit()
+        return new_like.to_dict();
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/like_form.py
+++ app/forms/like_form.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from wtforms.validators import DataRequired, ValidationError
 
-class BackingForm(FlaskForm):
+class LikeForm(FlaskForm):
     project_id = IntegerField('project_id', validators=[DataRequired()])
     user_id = IntegerField('user_id', validators=[DataRequired()])
